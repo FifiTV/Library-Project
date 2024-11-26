97a7235d136ba69555dc64f47fefa623c63c827b
eryk <rakaeryk@gmail.com>
2024-11-26 19:48:38 +0100

    Adjusting naming convention for db

--- models/user.go
+++ models/user.go
@@ -4,8 +4,8 @@ type User struct {
 	Id        int    `firestore:"id"`
 	Email     string `firestore:"email"`
 	Password  string `firestore:"password"`
-	FirstName string `firestore:"firstname"`
-	LastName  string `firestore:"lastname"`
+	FirstName string `firestore:"firs_tname"`
+	LastName  string `firestore:"last_name"`
 	Age       int    `firestore:"age"`
 	Role      int    `firestore:"role"`
 }
