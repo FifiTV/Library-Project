a245ed5166b6c246031fb670d43ed205f1066db9
eryk <rakaeryk@gmail.com>
2024-11-27 21:37:05 +0100

    Change of book list display

--- models/book.go
+++ models/book.go
@@ -3,11 +3,11 @@ package models
 import "time"
 
 type Book struct {
-	Title         string    `firestore:"title"`
-	Author        string    `firestore:"author"`
-	Pages         int       `firestore:"pages"`
-	Id            int       `firestore:"id"`
-	Description   string    `firebase:"description"`
-	Cover         string    `firebase:"cover"`
-	DateOfRelease time.Time `firebase:"date_of_release"`
+	Title       string    `firestore:"title"`
+	Author      string    `firestore:"author"`
+	Pages       int       `firestore:"pages"`
+	Id          int       `firestore:"id"`
+	Description string    `firebase:"description"`
+	Cover       string    `firebase:"cover"`
+	PublishedAt time.Time `firebase:"published_at"`
 }
