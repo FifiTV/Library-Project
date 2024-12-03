7b518dcc005c1c0619cd672f5b7d8aebe468520a
FifiTV <filip.fit1302@gmail.com>
2024-12-03 22:42:38 +0100

    Add book and correct an error page

--- helpers/routes.go
+++ helpers/routes.go
@@ -28,10 +28,15 @@ func Routes(app *fiber.App) {
 		middleware.RoleGuard(middleware.User),
 		controllers.GetHistoryPage)
 
-	app.Get("/addBook", controllers.GetAddBookPage)
-	app.Post("/addBook", func(c *fiber.Ctx) error {
-		return controllers.AddNewBookToLibrary(c, initializers.Client)
-	})
+	app.Get("/addBook", middleware.AuthGuard,
+		middleware.RoleGuard(middleware.Librarian),
+		controllers.GetAddBookPage)
+	app.Post("/addBook",
+		middleware.AuthGuard,
+		middleware.RoleGuard(middleware.Librarian),
+		func(c *fiber.Ctx) error {
+			return controllers.AddNewBookToLibrary(c, initializers.Client)
+		})
 
 	app.Post("/bookdetails/:id", middleware.AuthGuard, func(c *fiber.Ctx) error {
 		return controllers.BorrowBook(c, initializers.Client)
